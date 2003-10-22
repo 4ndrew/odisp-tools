aa229da7f2363bd1e59114da95bb40b2db3bd8cd
Valentin Alexeev <valentin.alekseev@gmail.com>
2003-10-22 20:49:38 +0000

    java.util.logging formatter that styles message into a one line string

---
+++ com/novel/tools/OneLineFormatter.java
@@ -0,0 +1,35 @@
+package com.novel.tools.log;
+
+import java.util.logging.*;
+import java.util.Locale;
+import java.util.Date;
+import java.text.DateFormat;
+
+/** Форматирование сообщения в одну строку
+* @author Валентин А. Алексеев
+* @author НПП "Новел-ИЛ"
+* @version $Id: OneLineFormatter.java,v 1.1 2003/10/22 20:49:38 valeks Exp $
+*/
+public class OneLineFormatter extends Formatter {
+    /** Выполнить форматирование записи. Выходная строка в виде:
+    * HH:MM:SS LOGLEVEL CLASSNAME.METHOD: MESSAGE. PARAMETERS+
+    * @param record запись для анализа
+    * @return отформатированная строка
+    */
+    public String format(LogRecord record){
+	String result = "";
+	result+=DateFormat.getDateInstance(DateFormat.SHORT, Locale.getDefault()).format(new Date(record.getMillis()))+" ";
+	result+=record.getLevel()+"\t";
+	result+=record.getSourceClassName().substring(record.getSourceClassName().lastIndexOf(".")+1)+".";
+	result+=record.getSourceMethodName()+": ";
+	result+=record.getMessage()+".";
+	if(record.getParameters() != null &&record.getParameters().length > 0){
+	    Object[] params = record.getParameters();
+	    for(int i = 0; i < params.length-1; i++)
+		result+=params[i].toString()+", ";
+	    result+=params[params.length].toString();
+	}
+	result+="\n";
+	return result;
+    }
+}
