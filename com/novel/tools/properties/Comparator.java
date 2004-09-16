2082f984abfda97dc11614f4f267e2a02da4ae58
Andrew Porokhin <andrew.porokhin@gmail.com>
2004-09-16 16:02:46 +0000

    Visual interface changed.

--- com/novel/tools/properties/Comparator.java
+++ com/novel/tools/properties/Comparator.java
@@ -9,7 +9,7 @@ import java.util.Properties;
  * 
  * @author <a href="dron@novel-il.ru">Your_Realname</a>
  * @author (C) 2004 îðð "îÏ×ÅÌ-éì"
- * @version $Id: Comparator.java,v 1.1 2004/09/16 12:02:56 dron Exp $
+ * @version $Id: Comparator.java,v 1.2 2004/09/16 16:02:46 dron Exp $
  */
 public class Comparator {
   public static void main(String[] args) {
@@ -18,13 +18,16 @@ public class Comparator {
       return;
     }
     
+    System.out.println("Input file1:\n" + args[0]);
+    System.out.println("Input file2:\n" + args[1]);
+    
     Properties prop1 = new Properties();
     Properties prop2 = new Properties();
     try {
       prop1.load(new FileInputStream(new File(args[0])));
     	prop2.load(new FileInputStream(new File(args[1])));
     } catch (Exception e) {
-      System.err.println("IO Error." + e);
+      System.err.println("Error: " + e);
       return;
     }
     
@@ -36,6 +39,7 @@ public class Comparator {
         System.out.println("  key: " + buff);
       }
     }
+    System.out.println("=======================================");
     it = prop2.keySet().iterator();
     System.out.println("=== !contains(file1) && contains(file2)");
     while (it.hasNext()) {
@@ -44,5 +48,6 @@ public class Comparator {
         System.out.println("  key: " + buff);
       }
     }
+    System.out.println("=======================================");
   }
 }
