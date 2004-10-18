1fbb2e65a0c41c3fb1bdc88f8c0dc29b9ce46490
Boris Volkovyski <boris.volkovyski@mac.com>
2004-10-18 13:45:49 +0000

    *** empty log message ***

--- org/valabs/tplParser/tplProcessor.java
+++ org/valabs/tplParser/tplProcessor.java
@@ -6,11 +6,12 @@ import java.io.IOException;
 import java.io.PrintStream;
 
 
-/** TODO: insert comment there
+/**
+ * TODO: insert comment there
  * 
- * @author <a href="boris@novel-il.ru">Волковыский Борис В.</a>
+ * @author <a href="boris@novel-il.ru">Волковыский Борис В. </a>
  * @author (C) 2004 НПП "Новел-ИЛ"
- * @version $Id: tplProcessor.java,v 1.1 2004/10/18 13:15:41 boris Exp $
+ * @version $Id: tplProcessor.java,v 1.2 2004/10/18 13:45:49 boris Exp $
  */
 public class tplProcessor {
 
@@ -35,10 +36,10 @@ public class tplProcessor {
   private PrintStream javaWriter = null;
   
   /**
-	 * 
-	 * @param tplName
-	 * @param javaName
-	 */
+   * 
+   * @param tplName
+   * @param javaName
+   */
 	tplProcessor(String tplName, String javaName){
 	  try {
       tplReader = new BufferedReader(new FileReader(tplName));
@@ -75,107 +76,65 @@ public class tplProcessor {
    * @param line
    */
   private void parseTagLine(String tagLine) {
-    tagNAME(tagLine);
-    tagIMPORT(tagLine);
-    tagAUTHOR(tagLine);
-    tagDESC(tagLine);
-    tagFIELD(tagLine);
-    tagFCHECK(tagLine);
-    tagFDESC(tagLine);
-    tagDEFORIGIN(tagLine);
-    tagDEFDEST(tagLine);
-    tagDEFID(tagLine);
-    tagDEFROUTABLE(tagLine);
-    tagDEFOOB(tagLine);
-    tagNOTAG(tagLine);
-    tagCVSid(tagLine);
-  }
-
-  private void tagNAME(String tagLine) {
     if (tagLine.startsWith("NAME")){
       String[] s = tagLine.split(" ");
       packageName = s[1];
       messageName = s[2];
       actionName = s[3];
-    }    
-  }
-  
-  private void tagCVSid(String tagLine) {
+    }
+
     if (tagLine.startsWith("$")){
       tagCVSidstrings = tagLine;
     }    
-  }
 
-  private void tagIMPORT(String tagLine) {
     if (tagLine.startsWith("IMPORT")){
       tagIMPORTstrings += tagLine.split(" ")[1];
     }    
-  } 
-  
-  private void tagAUTHOR(String tagLine) {
+
     if (tagLine.startsWith("AUTHOR")){
       tagAUTHORstrings += " * @author " + tagLine.substring(8) + "\n";
     }    
-  }
 
-  private void tagDESC(String tagLine) {
     if (tagLine.startsWith("DESC")){
       tagDESCstrings += " * " + tagLine.substring(6) + "\n";
     }    
-  }
-  
-  private void tagFIELD(String tagLine) {
-    if (tagLine.startsWith("FIELD")){
+
+    if (tagLine.startsWith("FIELD")){//TODO
       tagFIELDstrings += tagLine + "\n";
     }    
-  }
-  
-  private void tagNOTAG(String tagLine) {
-    if (!tagLine.startsWith("#")){//комментарии пропускаем
-      tagNOTAGstrings += tagLine + "\n";
-    }
-  }
 
-  private void tagDEFOOB(String tagLine) {
-    if (tagLine.startsWith("DEFOOB")){
-      tagDEFOOBstrings += tagLine + "\n";
-    }    
-  }
+    if (tagLine.startsWith("FCHECK")){//TODO
+      tagFCHECKstrings += tagLine + "\n";
+    }   
+    
+    if (tagLine.startsWith("FDESC")){//TODO
+      tagFDESCstrings += tagLine + "\n";
+    } 
 
-  private void tagDEFROUTABLE(String tagLine) {
-    if (tagLine.startsWith("DEFROUTABLE")){
-      tagDEFROUTABLEstrings += tagLine + "\n";
-    }    
-  }
+    if (tagLine.startsWith("DEFORIGIN")){//TODO
+      tagDEFORIGINstrings += tagLine + "\n";
+    } 
 
-  private void tagDEFID(String tagLine) {
-    if (tagLine.startsWith("DEFID")){
-      tagDEFIDstrings += tagLine + "\n";
-    }    
-  }
-
-  private void tagDEFDEST(String tagLine) {
-    if (tagLine.startsWith("DEFDEST")){
+    if (tagLine.startsWith("DEFDEST")){//TODO
       tagDEFDESTstrings += tagLine + "\n";
     }    
-  }
 
-  private void tagDEFORIGIN(String tagLine) {
-    if (tagLine.startsWith("DEFORIGIN")){
-      tagDEFORIGINstrings += tagLine + "\n";
-    }    
-  }
+    if (tagLine.startsWith("DEFID")){//TODO
+      tagDEFIDstrings += tagLine + "\n";
+    }  
 
-  private void tagFDESC(String tagLine) {
-    if (tagLine.startsWith("FDESC")){
-      tagFDESCstrings += tagLine + "\n";
+    if (tagLine.startsWith("DEFROUTABLE")){//TODO
+      tagDEFROUTABLEstrings += tagLine + "\n";
     }    
-  }
 
-  private void tagFCHECK(String tagLine) {
-    if (tagLine.startsWith("FCHECK")){
-      tagFCHECKstrings += tagLine + "\n";
+    if (tagLine.startsWith("DEFOOB")){//TODO
+      tagDEFOOBstrings += tagLine + "\n";
     }    
+    
+    if (!tagLine.startsWith("#")){//TODO
+      //комментарии пропускаем
+      tagNOTAGstrings += tagLine + "\n";
+    }
   }
 
 }
